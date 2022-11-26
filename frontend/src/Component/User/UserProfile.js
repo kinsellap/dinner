e1bc4c92e8936a0bfb1defbfa00cd4407fc6fa23
kinsellap <d15123543@mytudublin.ie>
2022-11-26 14:41:33 +0000

    Initial profile

--- frontend/src/Component/User/UserProfile.js
+++ frontend/src/Component/User/UserProfile.js
@@ -1,7 +1,8 @@
 import React, { useState, useContext, useEffect } from "react";
 import { UserContext } from "../../Service/UserProvider";
-import { checkAuthFailure, getErrorDetails } from "../../Utils/ErrorUtils"
-import { getCountRecipesUpdated, getCountRecipesAdded,deleteUser, updateUser } from "../../Service/ApiService";
+import { checkAuthFailure, getErrorDetails } from "../../Utils/ErrorUtils";
+import { capitaliseFirstLetter } from "../../Utils/StringUtils";
+import { getCountRecipesUpdated, getCountRecipesAdded, deleteUser, updateUser } from "../../Service/ApiService";
 import { setAuthenticatedUser, setAuthToken } from "../../Service/SessionService";
 import M from 'materialize-css'
 
@@ -17,41 +18,53 @@ function UserProfile(props) {
                     (res) => {
                         if (res.data.length !== 0) {
                             setCountRecipesAdded(res.data);
-                        }  
+                        }
                     })
                 .catch((err) => {
                     M.toast({ html: `There was an error loading the added recipe count ${getErrorDetails(err)}`, classes: 'red' })
                     console.log(err)
                 })
         };
-        if(loggedInUser){
+        if (loggedInUser) {
             userCountRecipesAdded(loggedInUser._id);
         }
-    }, [countRecipesAdded,loggedInUser]);
+    }, [countRecipesAdded, loggedInUser]);
 
     useEffect(() => {
         const userCountRecipesUpdated = async (userId) => {
             await getCountRecipesUpdated(userId)
                 .then(
-                    (res) => {           
+                    (res) => {
                         if (res.data.length !== 0) {
                             setCountRecipesUpdated(res.data);
-                        }  
+                        }
                     })
                 .catch((err) => {
                     M.toast({ html: `There was an error loading the updated recipe count ${getErrorDetails(err)}`, classes: 'red' })
                     console.log(err)
                 })
         };
-        if(loggedInUser){
+        if (loggedInUser) {
             userCountRecipesUpdated(loggedInUser._id);
         }
-    }, [countRecipesAdded,loggedInUser]);
+    }, [countRecipesAdded, loggedInUser]);
 
-    return (<div>
-        {countRecipesAdded}
-        {countRecipesUpdated}
-    </div>);
+    return (
+        <div className="row">
+            <div className="col s12">
+                <h4 className="teal-text text-lighten-2">Hello {capitaliseFirstLetter(loggedInUser?.first_name)}</h4>
+                <div className="row">
+                    <div className="col s6 center">
+                        <h5>  {countRecipesAdded}</h5>
+                        <p>Recipes Added</p>
+                    </div>
+                    <div className="col s6 center">
+                    <h5>  {countRecipesUpdated}</h5>
+                       <p>Recipes Updated</p>
+                    </div>
+                </div>
+            </div>
+        </div>);
 }
 
 export default UserProfile;
--- frontend/src/Utils/StringUtils.js
+++ frontend/src/Utils/StringUtils.js
@@ -1,3 +1,6 @@
 export const isNotEmpty = (query) => query !== undefined && query !== "" && query !== null;
 
 export const isAnInteger = (content) => isNotEmpty(content) && /^\d+$/.test(content);
+
+export const capitaliseFirstLetter = (name) => name?.charAt(0).toUpperCase() + name?.slice(1);
+  
